fcad0e05867679e9bce058e2a1474e025b3aaee6
Dhyno <rinosaputra133@gmail.com>
2022-07-02 03:10:40 +0700

    update

--- simple_project/calculator.asm
+++ simple_project/calculator.asm
@@ -12,11 +12,14 @@ section .data
 	input_msg db 'your input: '
 	len_input_msg equ $ -input_msg
 
-	input_num1 db 'input first number: ',0xa
+	input_num1 db 'input first number: '
 	len_input_num1 equ $ -input_num1
 
-	input_num2 db 'input second number: ',0xa
+	input_num2 db 'input second number: '
 	len_input_num2 equ $ -input_num2
+
+	int_num1 dd 0
+	int_num2 dd 0
 
 	error_msg db 'operator is not valid',0xa,0xd
 	len_error_msg equ $ -error_msg
@@ -24,12 +27,24 @@ section .data
 	msg db 'operator is vaid',0xa,0xd
 	len_msg equ $ -msg
 
+	msg_add db 'in add',0xa
+	len_msg_add equ $ -msg_add
+
+	msg_sub db 'in sub',0xa
+	len_msg_sub equ $ -msg_sub
+
+	msg_div db 'in div',0xa
+	len_msg_div equ $ -msg_div
+
+	msg_mul db 'in mul',0xa
+	len_msg_mul equ $ -msg_mul
+
 section .bss
 	;data here
 	operator resb 1
 	trash resb 1
-	num1 resb 4
-	num2 resb 4
+	str_num1 resb 4
+	str_num2 resb 4
 
 section .text
 	global _start
@@ -55,6 +70,8 @@ _start:
 	mov edx,1
 	int 0x80
 
+
+	;validation for input
 	cmp byte[operator],0x61	; char a
 	jne next1_operator
 	jmp valid
@@ -82,12 +99,8 @@ invalid_operator:
 	jmp end
 
 valid:
+
 	call clear_buffer
-	; cmp byte [ecx], 10
-	; jz input_number
-	; mov ecx, trash
-    ; jmp valid
-; input_number:
 
 	mov eax,4				;sys_write
 	mov ebx,1				;file descriptor std_out
@@ -97,17 +110,9 @@ valid:
 
 	mov eax,3				;sys_read
 	mov ebx,0				;file desceiptor stdin
-	mov ecx,num1
+	mov ecx,str_num1
 	mov edx,4
 	int 0x80
-
-	mov eax,4				;sys_write
-	mov ebx,1				;file descriptor std_out
-	mov ecx,num1
-	mov edx,4
-	int 0x80
-
-	call clear_buffer
 
 	mov eax,4				;sys_write
 	mov ebx,1				;file descriptor std_out
@@ -117,21 +122,35 @@ valid:
 
 	mov eax,3				;sys_read
 	mov ebx,0				;file desceiptor stdin
-	mov ecx,num2
+	mov ecx,str_num2
 	mov edx,4
 	int 0x80
 
-	mov eax,4				;sys_write
-	mov ebx,1				;file descriptor std_out
-	mov ecx,num2
-	mov edx,4
-	int 0x80
+	;convert string to number
+	mov al
 
-	; mov eax,4				;sys_write
-	; mov ebx,1				;file descriptor std_out
-	; mov ecx,msg
-	; mov edx,len_msg
-	; int 0x80
+
+	cmp byte[operator],0x61	; char a
+	jne _sub
+
+_add:
+	call add_number
+	jmp end
+
+_sub:
+	cmp byte[operator],0x73 ;char s
+	jne _mul
+	call sub_number
+	jmp end
+_mul:
+	cmp byte[operator],0x6d ;char m
+	jne _div
+	call mul_number
+	jmp end
+_div:
+	call div_number
+
+
 
 end:
 	;sys_exit kernel
@@ -142,6 +161,39 @@ end:
 clear_buffer:
 	mov eax,3				;sys_read
 	mov ebx,0
+	mov ecx,trash
 	mov edx,1
 	int 0x80
 	ret
+
+add_number:
+	mov eax,4				;sys_write
+	mov ebx,1				;file descriptor std_out
+	mov ecx,msg_add
+	mov edx,len_msg_add
+	int 0x80
+	ret
+
+sub_number:
+	mov eax,4				;sys_write
+	mov ebx,1				;file descriptor std_out
+	mov ecx,msg_sub
+	mov edx,len_msg_sub
+	int 0x80
+	ret
+
+div_number:
+	mov eax,4				;sys_write
+	mov ebx,1				;file descriptor std_out
+	mov ecx,msg_div
+	mov edx,len_msg_div
+	int 0x80
+	ret	
+
+mul_number:
+	mov eax,4				;sys_write
+	mov ebx,1				;file descriptor std_out
+	mov ecx,msg_mul
+	mov edx,len_msg_mul
+	int 0x80
+	ret	
